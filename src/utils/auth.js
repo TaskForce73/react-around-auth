e9377760c00b931f2e406b82af3ea2ef742fd66c
Anton Filatov <antfilatov91@gmail.com>
2022-06-25 21:30:48 +0300

    project 14 final

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,16 +3,14 @@ export const BASE_URL = 'https://register.nomoreparties.co';
 const checkResponse = (res) =>
   res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
 
-  // Hi Mahmud! Nice to meet you again! I have a little question about this red mistake. As I know the private functions is a privelegy of the class instances, but I don't use them here in this module, so if i just right something like "const _checkResponse or _checkResponse ()" it will be a mistake, won't it?  So, what should I do here?) 
-
-export const register = ( password, email) => {
+export const register = (password, email) => {
   return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ password, email}),
+    body: JSON.stringify({ password, email }),
   }).then(checkResponse);
 };
 
@@ -20,10 +18,10 @@ export const authorize = (password, email) => {
   return fetch(`${BASE_URL}/signin`, {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({password, email})
+    body: JSON.stringify({ password, email }),
   }).then(checkResponse);
 };
 
@@ -32,9 +30,9 @@ export const validateToken = (token) => {
     return fetch(`${BASE_URL}/users/me`, {
       method: 'GET',
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`,
+        Authorization: `Bearer ${token}`,
       },
     }).then(checkResponse);
   }
